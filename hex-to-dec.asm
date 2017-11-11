812dbe8c3b4e11989d230330302d2d664f8a44d6
Torment <kaleshsingh96@gmail.com>
2017-11-10 22:10:59 -0500

    Added code to get input from the user

--- hex-to-dec.asm
+++ hex-to-dec.asm
@@ -1,9 +1,15 @@
 .data
-	error_msg: .asciiz "Invalid hexadecimal number." 
+	error_msg:	.asciiz "Invalid hexadecimal number." 
+	input_str:	.space 8 	# we need 8 bytes to read in at most 8 ascii characters
+	prompt:		.asciiz "Please enter a 8-digit or smaller hexadecimal number: "
 .text
+	main:
 
 	# Get user input
-
+		li $v0, 8		# Tells system to get user input from the keyboard as text
+		la $a0, input_str
+		li $a1, 8		# Tells the system the maximum number of bytes to read		
+	
 
 	# Validate user input
 
